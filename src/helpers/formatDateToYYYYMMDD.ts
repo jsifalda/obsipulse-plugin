0186179dd4bcdf84db20e5e6f5e09adfb9cef432
jsifalda <sifalda.jiri@gmail.com>
2024-11-01 15:27:31 +0100

    refactor: better js date formatting

--- src/helpers/formatDateToYYYYMMDD.ts
+++ src/helpers/formatDateToYYYYMMDD.ts
@@ -1,18 +1,18 @@
 export function formatDateToYYYYMMDD(date: Date) {
-  // Extracting individual components
-  const year = date.getFullYear()
-  let month = String(date.getMonth() + 1) // Months are zero-based!
-  let day = String(date.getDate())
+  // OLD WAY:
+  // const year = date.getFullYear()
+  // let month = String(date.getMonth() + 1)
+  // let day = String(date.getDate())
 
-  // Ensuring two-digit formats for month and day
-  if (+month < 10) {
-    month = '0' + month
-  }
+  // if (+month < 10) {
+  //   month = '0' + month
+  // }
 
-  if (+day < 10) {
-    day = '0' + day
-  }
+  // if (+day < 10) {
+  //   day = '0' + day
+  // }
+  // return `${year}-${month}-${day}`
 
-  // Concatenating components in YYYY-MM-DD format
-  return `${year}-${month}-${day}`
+  // NEW WAY:
+  return date.toISOString().slice(0, 10)
 }
